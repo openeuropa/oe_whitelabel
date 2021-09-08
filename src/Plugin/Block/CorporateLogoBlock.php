ea19c28438401c54cd56821cfab46a398f3348b1
Abel Santos <abel.santos.corral@gmail.com>
2021-09-08 16:50:11 +0200

    OEL-448: Pre-release of corporate logo block.

---
+++ src/Plugin/Block/CorporateLogoBlock.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_whitelabel\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Exposes a block with EU/EC logo (Corporate Block).
+ *
+ * @Block(
+ *  id = "whitelabel_logo_block",
+ *  admin_label = @Translation("Corporate Logo Block"),
+ *  category = @Translation("Blocks"),
+ * )
+ */
+class CorporateLogoBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration(): array {
+    return [
+      'source_logo' => 'ec',
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state): array {
+    $form = parent::blockForm($form, $form_state);
+    $config = $this->getConfiguration();
+    $sourceLogoOptions = [
+      'eu' => $this->t('EU logo'),
+      'ec' => $this->t('EC logo'),
+    ];
+    $form['source_logo'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Logo Source'),
+      '#description' => $this->t('Please select the source for the logo.'),
+      '#options' => $sourceLogoOptions,
+      '#default_value' => $config['source_logo'],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state): void {
+    $this->setConfigurationValue('source_logo', $form_state->getValue('source_logo'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    // @todo Yet to be developed.
+    return [
+      '#markup' => $this->t('Logo here'),
+    ];
+  }
+
+}
