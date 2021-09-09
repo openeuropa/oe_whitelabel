10320dd5329ba4a10d3aa9fa8bb0af17aa6316c4
Abel Santos <abel.santos.corral@gmail.com>
2021-09-09 09:56:04 +0200

    OEL-448: Change name of module to oe_whitelabel_logo.

---
+++ modules/oe_whitelabel_logo/src/Plugin/Block/CorporateLogoBlock.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_whitelabel_logo\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Component\Utility\NestedArray;
+
+/**
+ * Exposes a block with EU/EC logo (Corporate Block).
+ *
+ * @Block(
+ *  id = "whitelabel_logo_block",
+ *  admin_label = @Translation("Corporate Logo Block"),
+ *  category = @Translation("Blocks"),
+ * )
+ */
+class CorporateLogoBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration(): array {
+    return [
+      'source_logo' => 'ec',
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state): array {
+    $form = parent::blockForm($form, $form_state);
+    $config = $this->getConfiguration();
+    $sourceLogoOptions = [
+      'eu' => $this->t('EU logo'),
+      'ec' => $this->t('EC logo'),
+    ];
+    $form['source_logo'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Logo Source'),
+      '#description' => $this->t('Please select the source for the logo.'),
+      '#options' => $sourceLogoOptions,
+      '#default_value' => $config['source_logo'],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state): void {
+    $this->setConfigurationValue('source_logo', $form_state->getValue('source_logo'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $cache = new CacheableMetadata();
+    $cache->addCacheContexts(['languages:language_interface']);
+
+    $config = $this->getConfiguration();
+    $cache->addCacheableDependency($config);
+
+    $build['#theme'] = 'oe_whitelabel_corporate_logo_block';
+
+    // @todo In function of language and logo source, prepare the necessary elements.
+    NestedArray::setValue($build, ['#corporate_footer', 'legal_navigation'], $config['source_logo']);
+    $cache->applyTo($build);
+
+    return $build;
+  }
+
+}
