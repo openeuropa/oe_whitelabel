3376f18c49aab3f4366a5950fc0dc0159f326e1f
drishu <aszilagyi@live.com>
2021-09-10 11:05:27 +0000

    OEL-448: Refactor block.

--- modules/oe_whitelabel_helper/src/Plugin/Block/CorporateLogoBlock.php
+++ modules/oe_whitelabel_helper/src/Plugin/Block/CorporateLogoBlock.php
@@ -38,7 +38,7 @@ class CorporateLogoBlock extends BlockBase implements ContainerFactoryPluginInte
   protected $languageManager;
 
   /**
-   * Construct the footer block object.
+   * Construct CorporateLogoBlock object.
    *
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
@@ -51,13 +51,7 @@ class CorporateLogoBlock extends BlockBase implements ContainerFactoryPluginInte
    * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
    *   The language manager.
    */
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    ConfigFactoryInterface $config_factory,
-    LanguageManagerInterface $language_manager
-    ) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory, LanguageManagerInterface $language_manager) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->configFactory = $config_factory;
     $this->languageManager = $language_manager;
@@ -66,11 +60,7 @@ class CorporateLogoBlock extends BlockBase implements ContainerFactoryPluginInte
   /**
    * {@inheritdoc}
    */
-  public static function create(
-    ContainerInterface $container,
-    array $configuration,
-    $plugin_id,
-    $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
       $configuration,
       $plugin_id,
@@ -127,30 +117,71 @@ class CorporateLogoBlock extends BlockBase implements ContainerFactoryPluginInte
     $cache->addCacheableDependency($config);
 
     $logo_path = drupal_get_path('module', 'oe_whitelabel_helper') . '/images/logos/' . $config['logo_source'];
-    $width = '290px';
+    $title = $this->configFactory->get('system.site')->get('name');
 
     if ($config['logo_source'] === 'eu') {
-      $uri = $logo_path . '/logo-eu--' . $language . '.normal.svg';
+      $build = $this->buildEuLogo($logo_path, $language, $title);
     }
     else {
-      $uri = $logo_path . '/logo--' . $language . '.svg';
+      $build = $this->buildEcLogo($logo_path, $language, $title);
     }
 
-    $config = $this->configFactory->get('system.site');
-    $title = $config->get('name');
+    $cache->applyTo($build);
+
+    return $build;
+  }
+
+  /**
+   * Build the EU logo.
+   *
+   * @param string $logo_path
+   *   The logo path.
+   * @param string $language
+   *   The language code.
+   * @param string $title
+   *   The site title.
+   *
+   * @return array
+   *   The render array.
+   */
+  protected function buildEuLogo(string $logo_path, string $language, string $title): array {
     $image = [
       '#theme' => 'image',
-      '#uri' => $uri,
-      '#width' => $width,
+      '#uri' => $logo_path . '/logo-eu--' . $language . '.normal.svg',
+      '#width' => '290px',
       '#alt' => $title,
       '#title' => $title,
     ];
-
-    $cache->addCacheableDependency($image);
-    $cache->applyTo($image);
-
     return [
-      'image' => $image,
+      '#type' => 'inline_template',
+      '#template' => '<picture><source media="(max-width: 25em)" srcset="{{ mobile }}">{{ image }}</picture>',
+      '#context' => [
+        'image' => $image,
+        'mobile' => $logo_path . '/logo-eu--' . $language . '.mobile.svg',
+      ],
+    ];
+  }
+
+  /**
+   * Build the EC logo.
+   *
+   * @param string $logo_path
+   *   The logo path.
+   * @param string $language
+   *   The language code.
+   * @param string $title
+   *   The site title.
+   *
+   * @return array
+   *   The render array.
+   */
+  protected function buildEcLogo(string $logo_path, string $language, string $title): array {
+    return [
+      '#theme' => 'image',
+      '#uri' => $logo_path . '/logo--' . $language . '.svg',
+      '#width' => '290px',
+      '#alt' => $title,
+      '#title' => $title,
     ];
   }
 
