06c9a02cc1e23460d4d5416110c8b0aafa6ff1de
Sergio Elvira Perez <sergioelviraperez@gmail.com>
2021-12-01 18:02:45 +0100

    OEL-601: Whitelabel search button modified to add input

--- modules/oe_whitelabel_search/src/Form/SearchForm.php
+++ modules/oe_whitelabel_search/src/Form/SearchForm.php
@@ -9,8 +9,6 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Template\Attribute;
-use Drupal\Core\Routing\TrustedRedirectResponse;
 
 /**
  * Configurable search form class.
@@ -69,7 +67,6 @@ class SearchForm extends FormBase {
         'placeholder' => $config['input']['placeholder'],
         'class' => [
           $config['input']['classes'],
-          'border-start-0',
           'rounded-0',
           'rounded-start',
         ],
@@ -78,65 +75,31 @@ class SearchForm extends FormBase {
       '#required' => TRUE,
     ];
 
-    // $button = [
-    //   '#type' => 'submit',
-    //   '#id' => 'button',
-    //   '#variant' => 'light',
-    //     '#attributes' => [
-    //       'class' => [
-    //         'border-start-0',
-    //         'rounded-0 rounded-end',
-    //         'd-flex',
-    //         'btn btn-light',
-    //         'btn-md',
-    //         'py-2',
-    //         $config['button']['classes'],
-    //       ],
-    //     ],
-    //   '#fields' => [
-    //     'settings' => [
-    //       'type' => 'submit',
-    //     ],
-    //     'attributes' => [
-    //       'class' => [
-    //         'border-start-0',
-    //         'rounded-0 rounded-end',
-    //         'd-flex',
-    //         'btn btn-light',
-    //         'btn-md',
-    //         'py-2',
-    //         $config['button']['classes'],
-    //       ],
-    //     ],
-    //   ],
-    // ];
-    // if ($config['button']['label_icon'] != 'icon') {
-    //   $button['#fields']['label'] = $this->t($config['button']['label']);
-    //   $button['#value'] = $this->t($config['button']['label']);
-    // }
-
     $button = [
-      '#type' => 'pattern',
+      '#type' => 'submit',
       '#id' => 'button',
       '#variant' => 'light',
+      '#pattern' => TRUE,
+      '#attributes' => [
+        'class' => [
+          'border-start-0',
+          'rounded-0',
+          'rounded-end',
+          'd-flex',
+          'btn',
+          'btn-light',
+          'btn-md',
+          'py-2',
+          $config['button']['classes'],
+        ],
+      ],
       '#fields' => [
         'settings' => [
           'type' => 'submit',
         ],
-        'attributes' => [
-          'id' => 'submit',
-          'class' => [
-            'border-start-0',
-            'rounded-0 rounded-end',
-            'd-flex',
-            'btn btn-light',
-            'btn-md',
-            'py-2',
-            $config['button']['classes'],
-          ],
-        ],
       ],
     ];
+
     if ($config['button']['label_icon'] != 'icon') {
       $button['#fields']['label'] = $this->t($config['button']['label']);
     }
@@ -160,7 +123,7 @@ class SearchForm extends FormBase {
       'display' => $config['view_options']['display'],
       'filter' => $config['input']['name'],
     ];
-    
+
     return $form;
   }
 
