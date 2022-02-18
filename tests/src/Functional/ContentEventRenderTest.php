6e2de17e54a4453c5903173a7279f666b9bda599
Gilmar Lima <gilmar.lima@novacomm-europa.eu>
2022-02-18 16:31:27 +0000

    OEL-1026: Update display view templates, update configuration and override content for inpage navigation.

--- tests/src/Functional/ContentEventRenderTest.php
+++ tests/src/Functional/ContentEventRenderTest.php
@@ -12,10 +12,8 @@ use Drupal\Tests\TestFileCreationTrait;
 
 /**
  * Tests that the Event content type renders correctly.
- *
- * @group batch1
  */
-class ContentEventRenderTest extends ContentRenderTestBase {
+class ContentEventRenderTest extends WhitelabelBrowserTestBase {
 
   use MediaTypeCreationTrait;
   use TestFileCreationTrait;
@@ -24,16 +22,13 @@ class ContentEventRenderTest extends ContentRenderTestBase {
    * {@inheritdoc}
    */
   public static $modules = [
-    'system',
-    'oe_whitelabel_helper',
-    'oe_starter_content_event',
     'oe_whitelabel_event',
   ];
   
   /**
    * A node to be rendered in diferent display views.
    *
-   * @var \Drupal\node\Entity\Node
+   * @var \Drupal\node\NodeInterface
    */
   protected $node;
 
@@ -44,7 +39,7 @@ class ContentEventRenderTest extends ContentRenderTestBase {
     parent::setUp();
 
     // Create a sample media entity to be embedded.
-    $image_file = File::create([
+    File::create([
       'uri' => $this->getTestFiles('image')[0]->uri,
     ])->save();
     $media_image = Media::create([
@@ -60,27 +55,29 @@ class ContentEventRenderTest extends ContentRenderTestBase {
     ]);
     $media_image->save();
     
-    // // Create a sample document media entity to be embedded.
-    // $text_file = File::create([
-    //   'uri' => $this->getTestFiles('text')[0]->uri,
-    // ])->save();
-    // $media_file = Media::create([
-    //   'bundle' => 'document',
-    //   'name' => 'Starter Image test',
-    //   'oe_media_file_type' = 'local',
-    //   'oe_media_file' => [
-    //     [
-    //       'target_id' => $text_file->id(),
-    //       'alt' => 'Starter Image test alt',
-    //       'title' => 'Starter Image test title',
-    //     ],
-    //   ],
-    // ]);
-    // $media_file->save();
+    // Create a sample document media entity to be embedded.
+    File::create([
+      'uri' => $this->getTestFiles('text')[0]->uri,
+    ])->save();
+    $media_document = Media::create([
+      'bundle' => 'document',
+      'name' => 'Event document test',
+      'oe_media_file_type' => 'local',
+      'oe_media_file' => [
+        [
+          'target_id' => 2,
+          'alt' => 'Event document alt',
+          'title' => 'Event document title',
+        ],
+      ],
+    ]);
+    $media_document->save();
 
-    // Create a Event node.
+    // Create a News node.
     /** @var \Drupal\node\Entity\Node $node */
-    $node = $this->getStorage('node')->create([
+    $node = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->create([
       'type' => 'oe_event',
       'title' => 'Test event node',
       'oe_summary' => 'http://www.example.org is a web page',
@@ -92,7 +89,7 @@ class ContentEventRenderTest extends ContentRenderTestBase {
       'uid' => 1,
       'status' => 1,
     ]);
-    // $node->set('oe_documents', [$media_file]);
+    $node->set('oe_documents', [$media_document]);
     $node->set('oe_featured_media', [$media_image]);
     $node->save();
     $this->node = $node;
@@ -102,19 +99,11 @@ class ContentEventRenderTest extends ContentRenderTestBase {
    * Tests that the Event page renders correctly in full display.
    */
   public function testEventRenderingFull(): void {
-    $this->drupalGet($this->node->toUrl());
-
     // Build node full view.
     $builder = \Drupal::entityTypeManager()->getViewBuilder('node');
     $build = $builder->view($this->node, 'full');
     $render = $this->container->get('renderer')->renderRoot($build);
-    $crawler = new Crawler($render->__toString());
-
-    // Assert content banner image.
-    $picture = $this->assertSession()->elementExists('css', 'img.card-img-top');
-    $image = $this->assertSession()->elementExists('css', 'img.rounded-1', $picture);
-    $this->assertStringContainsString('image-test.png', $image->getAttribute('src'));
-    $this->assertEquals('Starter Image test alt', $image->getAttribute('alt'));
+    $crawler = new Crawler((string) $render);
 
     // Assert content banner title.
     $content_banner = $crawler->filter('.bcl-content-banner');
@@ -122,36 +111,59 @@ class ContentEventRenderTest extends ContentRenderTestBase {
       'Test event node',
       trim($content_banner->filter('.card-title')->text())
     );
+    // Assert content banner image.
+    $image = $content_banner->filter('img');
+    $this->assertCount(1, $image);
+    $this->assertCount(1, $image->filter('.card-img-top'));
+    $this->assertStringContainsString(
+      'image-test.png',
+      trim($image->attr('src'))
+    );
+    $this->assertEquals('Starter Image test alt',
+      $image->attr('alt')
+    );
     // Assert content banner summary.
     $this->assertEquals(
       'http://www.example.org is a web page',
       trim($content_banner->filter('.oe-event__oe-summary')->text())
     );
+    // Assert inpage-navigation.
+    $this->assertEquals(
+      'Page content',
+      trim($crawler->filter('nav.bcl-inpage-navigation > h5')->text())
+    );
+    $inpage_links = $crawler->filter('nav.bcl-inpage-navigation > ul');
+    $this->assertCount(1, $inpage_links->filter('li'));
+    // Assert inpage-navigation first link.
+    $this->assertEquals(
+      'Content',
+      trim($inpage_links->filter('li:first-of-type')->text())
+    );
+
   }
 
   /**
    * Tests that the Event page renders correctly in teaser display.
    */
   public function testEventRenderingTeaser(): void {
-
-    $this->drupalGet($this->node->toUrl());
-
     // Build node teaser view.
     $builder = \Drupal::entityTypeManager()->getViewBuilder('node');
     $build = $builder->view($this->node, 'teaser');
     $render = $this->container->get('renderer')->renderRoot($build);
-    $crawler = new Crawler($render->__toString());
-
-    // Assert content banner image.
-    $picture = $this->assertSession()->elementExists('css', 'img.card-img-top');
-    $image = $this->assertSession()->elementExists('css', 'img.rounded-1', $picture);
-    $this->assertStringContainsString('image-test.png', $image->getAttribute('src'));
-    $this->assertEquals('Starter Image test alt', $image->getAttribute('alt'));
+    $crawler = new Crawler((string) $render);
 
     // Assert content banner title.
     $this->assertEquals(
       'Test event node',
       trim($crawler->filter('h5.card-title')->text())
+    );
+    // Assert content banner image.
+    $image = $crawler->filter('img');
+    $this->assertCount(1, $image);
+    $this->assertCount(1, $image->filter('.card-img-top'));
+    $this->assertStringContainsString(
+      'image-test.png',
+      trim($image->attr('src'))
     );
   }
 
