6479f0f6c2aac6d07d33700430d36f2618b30e73
Andreas Hennings <andreas@dqxtech.net>
2022-03-25 17:57:43 +0100

    OEL-1227: Create and place the ContentBannerBlock.

---
+++ modules/oe_whitelabel_helper/src/Plugin/Block/ContentBannerBlock.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_whitelabel_helper\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Provides a view mode block.
+ *
+ * @Block(
+ *   id = "oe_w_content_banner",
+ *   admin_label = @Translation("Content banner"),
+ *   context_definitions = {
+ *     "node" = @ContextDefinition("entity:node", label = @Translation("Node"))
+ *   },
+ * )
+ */
+class ContentBannerBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockAccess(AccountInterface $account): AccessResult {
+    /** @var \Drupal\node\NodeInterface $node */
+    $node = $this->getContextValue('node');
+
+    /** @var \Drupal\Core\Entity\EntityViewModeInterface|null $view_mode_object */
+    $view_mode_object = \Drupal::entityTypeManager()
+      ->getStorage('entity_view_display')
+      ->load('node.' . $node->bundle() . '.oe_w_content_banner');
+
+    return AccessResult::allowedIf($view_mode_object !== NULL);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    /** @var \Drupal\Core\Entity\EntityInterface $entity */
+    $entity = $this->getContextValue('node');
+    $view_builder = \Drupal::entityTypeManager()->getViewBuilder($entity->getEntityTypeId());
+    return $view_builder->view($entity, 'oe_w_content_banner');
+  }
+
+}
