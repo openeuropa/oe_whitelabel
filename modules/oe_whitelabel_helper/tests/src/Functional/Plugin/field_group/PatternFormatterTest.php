2ed94c0ff22e023c09b847aee17ff323a8902bf7
Sergio Elvira Perez <sergioelviraperez@gmail.com>
2022-04-01 02:30:23 +0200

    OEL-1297: Add test for the formatter.

---
+++ modules/oe_whitelabel_helper/tests/src/Functional/Plugin/field_group/PatternFormatterTest.php
@@ -0,0 +1,143 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_whitelabel_helper\Functional\Plugin\field_group;
+
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\field_group\Functional\FieldGroupTestTrait;
+
+/**
+ * Test the pattern field group formatter.
+ **/
+class PatternFormatterTest extends BrowserTestBase {
+
+  use FieldGroupTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'node',
+    'text',
+    'field_ui',
+    'field_group',
+    'oe_whitelabel_helper',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    parent::setUp();
+
+    // Enable oe_theme and set it as default.
+    $this->assertTrue($this->container->get('theme_installer')->install(['oe_whitelabel']));
+    $this->container->get('config.factory')
+      ->getEditable('system.theme')
+      ->set('default', 'oe_whitelabel')
+      ->save();
+
+    // Rebuild the ui_pattern definitions to collect the ones provided by
+    // oe_theme itself.
+    $this->container->get('plugin.manager.ui_patterns')->clearCachedDefinitions();
+
+    // Create test user.
+    $admin_user = $this->drupalCreateUser([
+      'access content',
+      'administer content types',
+      'administer node fields',
+      'administer node form display',
+      'administer node display',
+      'bypass node access',
+    ]);
+    $this->drupalLogin($admin_user);
+
+    // Create content type.
+    $this->drupalCreateContentType([
+      'name' => 'Test',
+      'type' => 'test',
+    ]);
+
+    /** @var \Drupal\Core\Entity\Display\EntityViewDisplayInterface $display */
+    $display = \Drupal::entityTypeManager()
+      ->getStorage('entity_view_display')
+      ->load('node.test.default');
+
+    // Create test fields.
+    $fields = [
+      'field_test_1' => 'Field 1',
+      'field_test_2' => 'Field 2',
+    ];
+    foreach ($fields as $field_name => $field_label) {
+      $field_storage = FieldStorageConfig::create([
+        'field_name' => $field_name,
+        'entity_type' => 'node',
+        'type' => 'text',
+      ]);
+      $field_storage->save();
+
+      $instance = FieldConfig::create([
+        'field_storage' => $field_storage,
+        'bundle' => 'test',
+        'label' => $field_label,
+      ]);
+      $instance->save();
+
+      // Set the field visible on the display object.
+      $display->setComponent($field_name, [
+        'label' => 'above',
+        'type' => 'text_default',
+      ]);
+    }
+
+    // Save display + create node.
+    $display->save();
+  }
+
+  /**
+   * Test description list pattern formatter.
+   */
+  public function testDescriptionListPatternFormatter() {
+    $data = [
+      'weight' => '1',
+      'children' => [
+        0 => 'field_test_1',
+        1 => 'field_test_2',
+      ],
+      'label' => 'Test label',
+      'format_type' => 'oe_whitelabel_helper_description_list_pattern',
+    ];
+    $group = $this->createGroup('node', 'test', 'view', 'default', $data);
+
+    $this->drupalCreateNode([
+      'type' => 'test',
+      'field_test_1' => [
+        ['value' => 'Content test 1'],
+      ],
+      'field_test_2' => [
+        ['value' => 'Content test 2'],
+      ],
+    ]);
+
+    field_group_group_save($group);
+
+    // Assert that fields are rendered using the field list horizontal pattern.
+    $this->drupalGet('node/1');
+
+    $element_selector = 'dl';
+    $this->assertSession()->elementExists('css', $element_selector);
+    $this->assertSession()->elementTextContains('css', $element_selector . ' dt:nth-child(1)', 'Field 1');
+    $this->assertSession()->elementTextContains('css', $element_selector . ' dd:nth-child(2)', 'Content test 1');
+    $this->assertSession()->elementTextContains('css', $element_selector . ' dt:nth-child(3)', 'Field 2');
+    $this->assertSession()->elementTextContains('css', $element_selector . ' dd:nth-child(4)', 'Content test 2');
+  }
+
+}
