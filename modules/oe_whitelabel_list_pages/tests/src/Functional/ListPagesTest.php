9542ba6cef864520205a3d28df4da3db82e7489a
escuriola <escuriola@gmail.com>
2022-04-05 09:40:35 +0200

    OEL-1246: Assert errors and facet filters.

--- modules/oe_whitelabel_list_pages/tests/src/Functional/ListPagesTest.php
+++ modules/oe_whitelabel_list_pages/tests/src/Functional/ListPagesTest.php
@@ -31,11 +31,10 @@ class ListPagesTest extends WhitelabelBrowserTestBase {
     $page = $this->getSession()->getPage();
     $assert_session = $this->assertSession();
 
-    // Create list for content type one.
     $this->drupalLogin($this->rootUser);
 
-    // Create some test nodes to index and search in.
-    for ($i = 0; $i < 11; $i++) {
+    // Create some test nodes.
+    for ($i = 0; $i < 12; $i++) {
       $date = new DrupalDateTime('20-10-2020');
       $values = [
         'title' => 'News number ' . $i,
@@ -48,29 +47,41 @@ class ListPagesTest extends WhitelabelBrowserTestBase {
       $node->save();
     }
 
+    // Index content.
     $list_source_factory = $this->container->get('oe_list_pages.list_source.factory');
     $item_list = $list_source_factory->get('node', 'oe_sc_news');
     $item_list->getIndex()->indexItems();
 
+    // Create list page for News.
     $this->drupalGet('/node/add/oe_list_page');
-
     $page->fillField('Title', 'News list page');
+
+    // Check that select bundle is required.
+    $page->pressButton('Save');
+    $assert_session->elementTextEquals('css', 'div.alert-content', 'Source bundle field is required.');
+
     $page->selectFieldOption('Source bundle', 'News');
     $page->pressButton('Save');
 
+    $assert_session->pageTextMatchesCount(10, '/News number/');
+    $assert_session->elementContains('css', 'h4.mb-0 > span', 'News list page');
+    $assert_session->elementContains('css', 'h4.mb-0', '(12)');
+    $assert_session->elementExists('css', 'div.bcl-offcanvas');
+    $assert_session->elementExists('css', 'nav > ul.pagination');
+    $assert_session->elementsCount('css', 'div.bcl-listing.bcl-listing--default-1-col > div.row > div.col > article > div.listing-item', '10');
+    $assert_session->elementsCount('css', 'hr', 2);
+    $assert_session->elementsCount('css', 'ul.pagination > li.page-item', 3);
+
+    // Add facets filters.
     $list_page = $this->getNodeByTitle('News list page');
     $this->drupalGet('node/' . $list_page->id() . '/edit');
     $page->checkField('Override default exposed filters');
     $page->checkField('emr_plugins_oe_list_page[wrapper][exposed_filters][oe_sc_news_title]');
     $page->pressButton('Save');
 
-    $assert_session->elementExists('css', 'div.bcl-offcanvas');
+    $assert_session->elementContains('css', 'h4.offcanvas-title', 'Filter options');
     $assert_session->elementExists('css', 'input[name="oe_sc_news_title"]');
     $assert_session->elementExists('css', 'button.btn-light > svg');
-    $assert_session->elementExists('css', 'nav > ul.pagination');
-    $assert_session->elementsCount('css', 'div.bcl-listing.bcl-listing--default-1-col > div.row > div.col > article > div.listing-item', '10');
-    $assert_session->elementsCount('css', 'hr', '2');
-    $assert_session->elementsCount('css', 'ul.pagination > li.page-item', '3');
 
   }
 
