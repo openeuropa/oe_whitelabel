d459b76f22cdebc58aff3f999c51223cae692d47
Andreas Hennings <andreas@dqxtech.net>
2022-04-10 23:08:50 +0200

    OEL-1227: Use dependency injection in ContentBannerBlock.

--- modules/oe_whitelabel_helper/src/Plugin/Block/ContentBannerBlock.php
+++ modules/oe_whitelabel_helper/src/Plugin/Block/ContentBannerBlock.php
@@ -6,7 +6,10 @@ namespace Drupal\oe_whitelabel_helper\Plugin\Block;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a view mode block.
@@ -20,7 +23,48 @@ use Drupal\Core\Session\AccountInterface;
  *   provider = "node",
  * )
  */
-class ContentBannerBlock extends BlockBase {
+class ContentBannerBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * Constructor.
+   *
+   * @param array $configuration
+   *   Plugin configuration.
+   * @param string $plugin_id
+   *   Plugin id.
+   * @param array $plugin_definition
+   *   Plugin definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager.
+   */
+  public function __construct(
+    array $configuration,
+    string $plugin_id,
+    array $plugin_definition,
+    EntityTypeManagerInterface $entity_type_manager
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -30,7 +74,7 @@ class ContentBannerBlock extends BlockBase {
     $node = $this->getContextValue('node');
 
     /** @var \Drupal\Core\Entity\EntityViewModeInterface|null $view_mode_object */
-    $view_mode_object = \Drupal::entityTypeManager()
+    $view_mode_object = $this->entityTypeManager
       ->getStorage('entity_view_display')
       ->load('node.' . $node->bundle() . '.oe_w_content_banner');
 
@@ -43,7 +87,7 @@ class ContentBannerBlock extends BlockBase {
   public function build(): array {
     /** @var \Drupal\node\NodeInterface $node */
     $node = $this->getContextValue('node');
-    return \Drupal::entityTypeManager()
+    return $this->entityTypeManager
       ->getViewBuilder('node')
       ->view($node, 'oe_w_content_banner');
   }
