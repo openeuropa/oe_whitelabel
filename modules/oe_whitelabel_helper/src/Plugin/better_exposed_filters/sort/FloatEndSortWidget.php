321e76b2fd8f1a22d030e10f8b7d6b78b88e65bf
escuriola <escuriola@gmail.com>
2022-04-27 17:52:16 +0200

    OEL-1374: Theme the sort form in the FloatEndSort widget.

--- modules/oe_whitelabel_helper/src/Plugin/better_exposed_filters/sort/FloatEndSortWidget.php
+++ modules/oe_whitelabel_helper/src/Plugin/better_exposed_filters/sort/FloatEndSortWidget.php
@@ -22,14 +22,17 @@ class FloatEndSortWidget extends DefaultWidget {
    */
   public function exposedFormAlter(array &$form, FormStateInterface $form_state) {
     parent::exposedFormAlter($form, $form_state);
-    $form['#attributes']['class'][] = 'float-lg-end';
-    $form['#attributes']['class'][] = 'd-none';
-    $form['#attributes']['class'][] = 'd-md-flex';
-    $form['#attributes']['class'][] = 'align-items-baseline';
+    $form['sort_bef_combine']['#wrapper_attributes']['class'][] = 'float-md-end';
+    $form['sort_bef_combine']['#wrapper_attributes']['class'][] = 'd-md-flex';
+    $form['sort_bef_combine']['#wrapper_attributes']['class'][] = 'align-items-center';
+    $form['sort_bef_combine']['#wrapper_attributes']['class'][] = 'mb-4';
+    $form['sort_bef_combine']['#wrapper_attributes']['class'][] = 'mb-md-0';
     $form['#attributes']['class'][] = 'col-md-6';
     $form['#attributes']['class'][] = 'col-lg-4';
     $form['#attributes']['class'][] = 'mt-3';
     $form['#attributes']['class'][] = 'mt-md-0';
+    $form['sort_bef_combine']['#attributes']['class'][] = 'ms-md-2';
+    $form['sort_bef_combine']['#label_attributes']['class'][] = 'text-nowrap mb-0';
   }
 
 }
