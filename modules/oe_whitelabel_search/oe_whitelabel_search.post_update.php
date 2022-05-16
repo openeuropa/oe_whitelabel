1d4d309cada10053ccf0f717bb95d178132c2ff1
escuriola <escuriola@gmail.com>
2022-05-16 13:48:05 +0200

    OEL-1367: Post update add the custom region value to the block.

--- modules/oe_whitelabel_search/oe_whitelabel_search.post_update.php
+++ modules/oe_whitelabel_search/oe_whitelabel_search.post_update.php
@@ -7,14 +7,15 @@
 
 declare(strict_types=1);
 
-use Drupal\oe_bootstrap_theme\ConfigImporter;
+use Drupal\block\Entity\Block;
 
 /**
  * Set the updated values to the search form block.
  */
 function oe_whitelabel_search_post_update_00001(&$sandbox) {
-  $configs = [
-    'block.block.oe_whitelabel_search_form',
-  ];
-  ConfigImporter::importMultiple('module', 'oe_whitelabel_search', '/config/post_updates/00001', $configs);
+  $block = Block::load('oe_whitelabel_search_form');
+  $settings = $block->get('settings');
+  $settings['form']['region'] = $block->getRegion();
+  $block->set('settings', $settings);
+  $block->save();
 }
