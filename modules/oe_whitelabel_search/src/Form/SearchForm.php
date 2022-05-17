fddf5756846a467be6c1849abcced0ce7ff145de
escuriola <escuriola@gmail.com>
2022-05-17 12:26:36 +0200

    OEL-1367: Fix default input value in the search form.

--- modules/oe_whitelabel_search/src/Form/SearchForm.php
+++ modules/oe_whitelabel_search/src/Form/SearchForm.php
@@ -48,6 +48,11 @@ class SearchForm extends FormBase {
     if (empty($config['input']['name'])) {
       return [];
     }
+    $input_value = '';
+    if (!empty($config['input']['name'])) {
+      $input_value = $this->getRequest()->get($config['input']['name']);
+    }
+
     $form_state->set('oe_whitelabel_search_config', $config);
 
     $form['#region'] = $config['form']['region'];
@@ -60,7 +65,7 @@ class SearchForm extends FormBase {
       '#margin_class' => 'mb-0',
       '#form_id' => $this->getFormId(),
       '#region' => $config['form']['region'],
-      '#default_value' => $config['input']['name'],
+      '#default_value' => $input_value,
       '#required' => TRUE,
     ];
 
