d09cfc64903929f297decc64112481650ccf69b1
escuriola <escuriola@gmail.com>
2022-06-02 18:05:40 +0200

    OEL-1540: Just print content if no inpage_navigation_links. Testing.

--- tests/src/Functional/ContentEventRenderTest.php
+++ tests/src/Functional/ContentEventRenderTest.php
@@ -153,6 +153,17 @@ class ContentEventRenderTest extends WhitelabelBrowserTestBase {
       'Event body',
       $crawler->filter('#oe-content-body p')->text()
     );
+
+    // Assert inpage_navigation not loaded if there is no body and documents.
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = \Drupal::entityTypeManager()->getStorage('node')->load($node->id());
+    $node->set('oe_documents', NULL);
+    $node->set('body', NULL);
+    $node->save();
+
+    $this->drupalGet('node/' . $node->id());
+
+    $this->assertSession()->elementNotExists('css', 'nav.bcl-inpage-navigation');
   }
 
   /**
