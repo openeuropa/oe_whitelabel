6c0e25379699155f170bc3041adf584baeaa154e
escuriola <escuriola@gmail.com>
2022-06-21 12:41:45 +0200

    OEL-1662: Adapt configs after moving the button template to the content banner.

--- modules/oe_whitelabel_starter_event/oe_whitelabel_starter_event.post_update.php
+++ modules/oe_whitelabel_starter_event/oe_whitelabel_starter_event.post_update.php
@@ -33,14 +33,19 @@ function oe_whitelabel_starter_event_post_update_00002(): void {
 }
 
 /**
- * Add registration URL field to full content view.
+ * Add registration URL field to event content_banner view.
  */
 function oe_whitelabel_starter_event_post_update_00003(): void {
-  ConfigImporter::importSingle(
+  $configs = [
+    'core.entity_view_display.node.oe_sc_event.full',
+    'core.entity_view_display.node.oe_sc_event.oe_w_content_banner',
+    'core.entity_view_display.node.oe_sc_event.teaser',
+  ];
+  ConfigImporter::importMultiple(
     'module',
     'oe_whitelabel_starter_event',
     '/config/post_updates/00003_add_registration_url_field',
-    'core.entity_view_display.node.oe_sc_event.full',
+    $configs,
     TRUE
   );
 }
