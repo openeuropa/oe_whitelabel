a400425645acaad9111b741fffb995f7570a9ab0
Francesco Sardara <francesco@tdgwebservices.com>
2022-06-24 21:44:45 +0200

    OEL-1653: Install new date formats in update path.

--- modules/oe_whitelabel_helper/oe_whitelabel_helper.post_update.php
+++ modules/oe_whitelabel_helper/oe_whitelabel_helper.post_update.php
@@ -19,3 +19,16 @@ function oe_whitelabel_helper_post_update_00001(): void {
   ];
   ConfigImporter::importMultiple('module', 'oe_whitelabel_helper', '/config/post_updates/00001_content_banner', $configs, TRUE);
 }
+
+/**
+ * Create date range formats.
+ */
+function oe_whitelabel_helper_post_update_00002(): void {
+  \Drupal::service('module_installer')->install(['daterange_compact']);
+
+  $configs = [
+    'daterange_compact.format.oe_whitelabel_date_only_short_month',
+    'daterange_compact.format.oe_whitelabel_date_time_long',
+  ];
+  ConfigImporter::importMultiple('module', 'oe_whitelabel_helper', '/config/post_updates/00002_date_range_formats', $configs, TRUE);
+}
