145680441ede41779e3cc651ce5c425198849e01
escuriola <escuriola@gmail.com>
2022-07-01 15:48:12 +0200

    OEL-1552: Testing required fields. Improve tests.

--- tests/src/Functional/ContentPersonRenderTest.php
+++ tests/src/Functional/ContentPersonRenderTest.php
@@ -31,15 +31,80 @@ class ContentPersonRenderTest extends WhitelabelBrowserTestBase {
    * Tests the person page.
    */
   public function testPersonPage(): void {
-    $node = $this->createExamplePerson();
+    $node = $this->createExamplePersonWithRequiredFieldsOnly();
     $this->drupalGet($node->toUrl());
     /** @var \Symfony\Component\BrowserKit\AbstractBrowser $client */
     $client = $this->getSession()->getDriver()->getClient();
     $crawler = $client->getCrawler();
 
-    // Select the content banner element.
+    // Assert required fields are present.
     $content_banner = $crawler->filter('.bcl-content-banner');
     $this->assertCount(1, $content_banner);
+
+    $this->assertEquals(
+      'Stefan Mayer',
+      $content_banner->filter('.card-title')->text()
+    );
+    $this->assertEquals(
+      'DG Test',
+      $content_banner->filter('.card-body > div.my-4 > span:nth-child(1)')->text()
+    );
+    $this->assertEquals(
+      'Director',
+      $content_banner->filter('.card-body > div.my-4 > span:nth-child(2)')->text()
+    );
+    $this->assertEquals(
+      'Germany',
+      $content_banner->filter('.card-body > div.my-4 > span:nth-child(3)')->text()
+    );
+
+    // Assert optional fields are not present.
+    // Assert no image is present.
+    $this->assertCount(0, $content_banner->filter('img'));
+
+    // Assert short description is not present.
+    $this->assertCount(0, $content_banner->filter('p'));
+
+    // Assert no social media links are present.
+    $this->assertCount(0, $content_banner->filter('a.standalone'));
+
+    $content = $crawler->filter('div.col-12.col-lg-10.col-xl-9.col-xxl-8');
+
+    // Assert additional information is not present.
+    $this->assertStringNotContainsString(
+      'Additional information',
+      $content->text()
+    );
+
+    // Assert documents are not present.
+    $this->assertStringNotContainsString(
+      'Related documents',
+      $content->text()
+    );
+
+    // Test person with all fields.
+    $node_with_all_fields = $this->createExamplePersonWithAllFields();
+    $this->drupalGet($node_with_all_fields->toUrl());
+    $crawler = $client->getCrawler();
+
+    $content_banner = $crawler->filter('.bcl-content-banner');
+
+    $this->assertEquals(
+      'Stefan Mayer',
+      $content_banner->filter('.card-title')->text()
+    );
+    $this->assertEquals(
+      'DG Test',
+      $content_banner->filter('.card-body > div.my-4 > span:nth-child(1)')->text()
+    );
+    $this->assertEquals(
+      'Director',
+      $content_banner->filter('.card-body > div.my-4 > span:nth-child(2)')->text()
+    );
+    $this->assertEquals(
+      'Germany',
+      $content_banner->filter('.card-body > div.my-4 > span:nth-child(3)')->text()
+    );
 
     // Assert content banner image.
     $image = $content_banner->filter('img');
@@ -47,57 +112,40 @@ class ContentPersonRenderTest extends WhitelabelBrowserTestBase {
     $this->assertCount(1, $image->filter('.card-img-top'));
     $this->assertStringContainsString(
       'image-test.png',
-      trim($image->attr('src'))
+      $image->attr('src')
     );
     $this->assertEquals('Starter Image test alt',
       $image->attr('alt')
     );
 
-    // Assert content banner content elements.
-    $this->assertEquals(
-      'Stefan Mayer',
-      trim($content_banner->filter('.card-title')->text())
-    );
-    $this->assertEquals(
-      'Director',
-      trim($content_banner->filter('.card-body > div.my-4 > span:nth-child(1)')->text())
-    );
-    $this->assertEquals(
-      'DG Test',
-      trim($content_banner->filter('.card-body > div.my-4 > span:nth-child(2)')->text())
-    );
-    $this->assertEquals(
-      'Germany',
-      trim($content_banner->filter('.card-body > div.my-4 > span:nth-child(3)')->text())
-    );
     $this->assertEquals(
       'This field is used to add a short biography of the person.',
-      trim($content_banner->filter('p')->text())
+      $content_banner->filter('p')->text()
     );
 
     $this->assertEquals(
       'Twitter profile',
-      trim($content_banner->filter('div.mb-2 > div.mb-3 > a.standalone')->text())
+      $content_banner->filter('div.mb-2 > div.mb-3 > a.standalone')->text()
     );
 
     $content = $crawler->filter('div.col-12.col-lg-10.col-xl-9.col-xxl-8');
 
     $this->assertEquals(
       'Additional information',
-      trim($content->filter('h2')->eq(0)->text())
+      $content->filter('h2')->eq(0)->text()
     );
     $this->assertEquals(
       'Additional information example field.',
-      trim($content->filter('div > p')->text())
+      $content->filter('div > p')->text()
     );
 
     $this->assertEquals(
       'Related documents',
-      trim($content->filter('h2')->eq(1)->text())
+      $content->filter('h2')->eq(1)->text()
     );
     $this->assertEquals(
       'Event document test',
-      trim($content->filter('.bcl-file p')->text())
+      $content->filter('.bcl-file p')->text()
     );
   }
 
@@ -105,7 +153,7 @@ class ContentPersonRenderTest extends WhitelabelBrowserTestBase {
    * Tests the person rendered in 'Teaser' view mode.
    */
   public function testPersonRenderingTeaser(): void {
-    $node = $this->createExamplePerson();
+    $node = $this->createExamplePersonWithAllFields();
     // Build node teaser view.
     $builder = \Drupal::entityTypeManager()->getViewBuilder('node');
     $build = $builder->view($node, 'teaser');
@@ -117,33 +165,59 @@ class ContentPersonRenderTest extends WhitelabelBrowserTestBase {
 
     $this->assertEquals(
       'Stefan Mayer',
-      trim($article->filter('h1.card-title')->text())
+      $article->filter('h1.card-title')->text()
     );
     $image = $article->filter('img');
     $this->assertCount(1, $image);
     $this->assertCount(1, $image->filter('.card-img-top'));
     $this->assertStringContainsString(
       'image-test.png',
-      trim($image->attr('src'))
-    );
-
-    $this->assertEquals(
-      'Director',
-      trim($article->filter('.card-body > div.my-3 > span:nth-child(1)')->text())
+      $image->attr('src')
     );
     $this->assertEquals(
       'DG Test',
-      trim($article->filter('.card-body > div.my-3 > span:nth-child(2)')->text())
+      $article->filter('.card-body > div.my-3 > span:nth-child(1)')->text()
+    );
+    $this->assertEquals(
+      'Director',
+      $article->filter('.card-body > div.my-3 > span:nth-child(2)')->text()
     );
   }
 
   /**
-   * Creates an example person node.
+   * Creates an example person node only with required fields.
    *
    * @return \Drupal\node\NodeInterface
    *   Person node.
    */
-  protected function createExamplePerson(): NodeInterface {
+  protected function createExamplePersonWithRequiredFieldsOnly(): NodeInterface {
+    // Create a Person node with required fields.
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->create([
+        'type' => 'oe_sc_person',
+        'oe_sc_person_first_name' => 'Stefan',
+        'oe_sc_person_last_name' => 'Mayer',
+        'oe_sc_person_country' => 'DE',
+        'oe_sc_person_occupation' => 'DG Test',
+        'oe_sc_person_position' => 'Director',
+        'uid' => 1,
+        'status' => 1,
+      ]);
+    $node->save();
+    return $node;
+  }
+
+  /**
+   * Creates an example person node with required and optional fields.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   Person node.
+   */
+  protected function createExamplePersonWithAllFields(): NodeInterface {
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = $this->createExamplePersonWithRequiredFieldsOnly();
     // Create a sample image media entity to be embedded.
     $image_file = File::create([
       'uri' => $this->getTestFiles('image')[0]->uri,
@@ -190,29 +264,13 @@ class ContentPersonRenderTest extends WhitelabelBrowserTestBase {
       ]);
     $document_reference->save();
 
-    // Create a Person node.
-    /** @var \Drupal\node\Entity\Node $node */
-    $node = \Drupal::entityTypeManager()
-      ->getStorage('node')
-      ->create([
-        'type' => 'oe_sc_person',
-        'oe_sc_person_first_name' => 'Stefan',
-        'oe_sc_person_last_name' => 'Mayer',
-        'oe_sc_person_country' => 'DE',
-        'oe_sc_person_occupation' => 'DG Test',
-        'oe_sc_person_position' => 'Director',
-        'oe_summary' => 'This field is used to add a short biography of the person.',
-        'oe_sc_person_additional_info' => 'Additional information example field.',
-        'oe_social_media_links' => [
-          [
-            'uri' => 'https://twiiter.com',
-            'title' => 'Twitter profile',
-            'link_type' => 'twitter',
-          ],
-        ],
-        'uid' => 1,
-        'status' => 1,
-      ]);
+    $node->set('oe_summary', 'This field is used to add a short biography of the person.');
+    $node->set('oe_sc_person_additional_info', 'Additional information example field.');
+    $node->set('oe_social_media_links', [
+      'uri' => 'https://twitter.com',
+      'title' => 'Twitter profile',
+      'link_type' => 'twitter',
+    ]);
     $node->set('oe_sc_person_documents', [$document_reference]);
     $node->set('oe_sc_person_image', [$media_image]);
     $node->save();
