24d68976a413c2ed33984bf32605101d01aab93d
drishu <aszilagyi@live.com>
2022-08-29 15:28:13 +0300

    OEL-1913: Test for translation.

--- modules/oe_whitelabel_paragraphs/tests/src/Kernel/Paragraphs/CarouselTest.php
+++ modules/oe_whitelabel_paragraphs/tests/src/Kernel/Paragraphs/CarouselTest.php
@@ -211,8 +211,8 @@ class CarouselTest extends ParagraphsTestBase {
           'caption_title' => 'Item 4',
           'caption' => 'Item description 4',
           'link' => [
-            'label' => '/',
-            'path' => 'CTA 4',
+            'label' => 'CTA 4',
+            'path' => '/',
           ],
           'image' => [
             'src' => file_create_url($en_file_2->getFileUri()),
@@ -221,6 +221,28 @@ class CarouselTest extends ParagraphsTestBase {
         ],
       ],
     ];
+    $assert->assertPattern($expected_values, $html);
+
+    // Assert paragraph rendering for Bulgarian version.
+    $html = $this->renderParagraph($paragraph, 'bg');
+    $expected_values['items'][0]['caption_title'] = 'BG Item 1';
+    $expected_values['items'][0]['image']['src'] = file_create_url($bg_file_1->getFileUri());
+    $expected_values['items'][0]['image']['alt'] = 'First image bg';
+    $expected_values['items'][1]['caption_title'] = 'BG Item 2';
+    $expected_values['items'][1]['caption'] = 'BG Item description 2';
+    $expected_values['items'][1]['link']['label'] = 'BG CTA 2';
+    $expected_values['items'][1]['image']['src'] = file_create_url($bg_file_2->getFileUri());
+    $expected_values['items'][1]['image']['alt'] = 'Second image bg';
+    $expected_values['items'][2]['caption_title'] = 'BG Item 3';
+    $expected_values['items'][2]['image']['src'] = file_create_url($bg_file_1->getFileUri());
+    $expected_values['items'][2]['image']['alt'] = 'First image bg';
+    $expected_values['items'][3]['caption_title'] = 'BG Item 4';
+    $expected_values['items'][3]['caption'] = 'BG Item description 4';
+    $expected_values['items'][3]['link']['path'] = 'http://www.example.com/';
+    $expected_values['items'][3]['link']['label'] = 'BG CTA 4';
+    $expected_values['items'][3]['image']['src'] = file_create_url($bg_file_2->getFileUri());
+    $expected_values['items'][3]['image']['alt'] = 'Second image bg';
+
     $assert->assertPattern($expected_values, $html);
   }
 
