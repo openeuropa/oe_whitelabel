6336633db6337a3322f0806008f4bf4a9ac62ffa
Francesco Sardara <francesco@tdgwebservices.com>
2022-08-31 18:12:16 +0200

    OEL-1657: Update configuration for new and existing installations.

--- modules/oe_whitelabel_starter_event/oe_whitelabel_starter_event.post_update.php
+++ modules/oe_whitelabel_starter_event/oe_whitelabel_starter_event.post_update.php
@@ -56,3 +56,18 @@ function oe_whitelabel_starter_event_post_update_00003(): void {
 function oe_whitelabel_starter_event_post_update_00004(): void {
   ConfigImporter::importSingle('module', 'oe_whitelabel_starter_event', '/config/post_updates/00004_teaser_location', 'core.entity_view_display.node.oe_sc_event.teaser');
 }
+
+/**
+ * Render the country flag for the event address.
+ */
+function oe_whitelabel_starter_event_post_update_00005(): void {
+  /** @var \Drupal\Core\Entity\Entity\EntityViewDisplay $view_display */
+  $view_display = \Drupal::entityTypeManager()->getStorage('entity_view_display')->load('node.oe_sc_event.full');
+  $component = $view_display->getComponent('oe_sc_event_location');
+  if ($component === NULL) {
+    return;
+  }
+
+  $component['settings']['show_country_flag'] = TRUE;
+  $view_display->setComponent('oe_sc_event_location', $component)->save();
+}
