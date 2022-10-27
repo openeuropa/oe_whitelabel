c47f985ee012eefb44e65dd93f23a3e814420f8a
Francesco Sardara <francesco@tdgwebservices.com>
2022-10-27 18:29:27 +0200

    OEL-2070: Import configuration for existing installations.

--- modules/oe_whitelabel_helper/oe_whitelabel_helper.post_update.php
+++ modules/oe_whitelabel_helper/oe_whitelabel_helper.post_update.php
@@ -7,6 +7,7 @@
 
 declare(strict_types =  1);
 
+use Drupal\Core\Config\FileStorage;
 use Drupal\oe_bootstrap_theme\ConfigImporter;
 
 /**
@@ -31,4 +32,12 @@ function oe_whitelabel_helper_post_update_00002(): void {
     'daterange_compact.format.oe_whitelabel_date_time_long',
   ];
   ConfigImporter::importMultiple('module', 'oe_whitelabel_helper', '/config/post_updates/00002_date_range_formats', $configs, TRUE);
+}
+
+/**
+ * Install the gallery item view mode if dependencies are met.
+ */
+function oe_whitelabel_helper_post_update_00003(): void {
+  $storage = new FileStorage(\Drupal::service('extension.list.module')->getPath('oe_whitelabel_helper') . '/config/post_updates/00003_gallery_formatter');
+  \Drupal::service('config.installer')->installOptionalConfig($storage);
 }
