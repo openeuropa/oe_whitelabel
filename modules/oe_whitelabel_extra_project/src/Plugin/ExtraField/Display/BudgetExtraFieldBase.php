680cc83a4df1cc0fb30e9ce1340aa643f68b0887
drishu <aszilagyi@live.com>
2023-04-12 09:22:57 +0300

    OEL-2267: Implement base class for the budget fields.

---
+++ modules/oe_whitelabel_extra_project/src/Plugin/ExtraField/Display/BudgetExtraFieldBase.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_whitelabel_extra_project\Plugin\ExtraField\Display;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\extra_field\Plugin\ExtraFieldDisplayFormattedBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Base class for budget fields.
+ */
+abstract class BudgetExtraFieldBase extends ExtraFieldDisplayFormattedBase implements ContainerFactoryPluginInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The entity view builder.
+   *
+   * @var \Drupal\Core\Entity\EntityViewBuilder
+   */
+  protected $viewBuilder;
+
+  /**
+   * OverallBudgetExtraField constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->viewBuilder = $entity_type_manager->getViewBuilder('node');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(ContentEntityInterface $entity) {
+    $field_budget = $this->getFieldBudget();
+    $field_eu_budget = $this->getFieldEuBudget();
+
+    if ($entity->get($field_budget)->isEmpty() && $entity->get($field_eu_budget)->isEmpty()) {
+      return [];
+    }
+
+    $build = [];
+    $display_options = [
+      'label' => 'hidden',
+      'type' => 'number_decimal',
+      'settings' => [
+        'thousand_separator' => '.',
+        'decimal_separator' => ',',
+        'scale' => 2,
+        'prefix_suffix' => TRUE,
+      ],
+    ];
+
+    if ($entity->get($field_eu_budget)->isEmpty()) {
+      // Fallback to old field.
+      $build[] = $this->viewBuilder->viewField($entity->get($field_budget), $display_options);
+    }
+    else {
+      // Render new field value.
+      $build[] = $this->viewBuilder->viewField($entity->get($field_eu_budget), $display_options);
+    }
+
+    return $build;
+  }
+
+  /**
+   * Returns the old budget field name.
+   *
+   * @return string
+   *   The budget field.
+   */
+  abstract protected function getFieldBudget(): string;
+
+  /**
+   * Returns the new budget field name.
+   *
+   * @return string
+   *   The budget field.
+   */
+  abstract protected function getFieldEuBudget(): string;
+
+}
--- modules/oe_whitelabel_extra_project/src/Plugin/ExtraField/Display/OverallBudgetExtraField.php
+++ modules/oe_whitelabel_extra_project/src/Plugin/ExtraField/Display/OverallBudgetExtraField.php
@@ -4,13 +4,6 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_whitelabel_extra_project\Plugin\ExtraField\Display;
 
-use Drupal\Core\Entity\ContentEntityInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\extra_field\Plugin\ExtraFieldDisplayFormattedBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 /**
  * Display overall budget.
  *
@@ -23,45 +16,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   visible = true
  * )
  */
-class OverallBudgetExtraField extends ExtraFieldDisplayFormattedBase implements ContainerFactoryPluginInterface {
-
-  use StringTranslationTrait;
-
-  /**
-   * The entity view builder.
-   *
-   * @var \Drupal\Core\Entity\EntityViewBuilder
-   */
-  protected $viewBuilder;
-
-  /**
-   * OverallBudgetExtraField constructor.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->viewBuilder = $entity_type_manager->getViewBuilder('node');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager')
-    );
-  }
+class OverallBudgetExtraField extends BudgetExtraFieldBase {
 
   /**
    * {@inheritdoc}
@@ -73,32 +28,15 @@ class OverallBudgetExtraField extends ExtraFieldDisplayFormattedBase implements 
   /**
    * {@inheritdoc}
    */
-  public function viewElements(ContentEntityInterface $entity) {
-    if ($entity->get('oe_project_budget')->isEmpty() && $entity->get('oe_project_eu_budget')->isEmpty()) {
-      return [];
-    }
-    $build = [];
+  protected function getFieldBudget(): string {
+    return 'oe_project_budget';
+  }
 
-    $display_options = [
-      'label' => 'hidden',
-      'type' => 'number_decimal',
-      'settings' => [
-        'thousand_separator' => '.',
-        'decimal_separator' => ',',
-        'scale' => 2,
-        'prefix_suffix' => TRUE,
-      ],
-    ];
-    if ($entity->get('oe_project_eu_budget')->isEmpty()) {
-      // Fallback to old field.
-      $build[] = $this->viewBuilder->viewField($entity->get('oe_project_budget'), $display_options);
-    }
-    else {
-      // Render new field value.
-      $build[] = $this->viewBuilder->viewField($entity->get('oe_project_eu_budget'), $display_options);
-    }
-
-    return $build;
+  /**
+   * {@inheritdoc}
+   */
+  protected function getFieldEuBudget(): string {
+    return 'oe_project_eu_budget';
   }
 
 }
