1b3bb21e657f3dc861bd612267c39870e3700ba5
drishu <aszilagyi@live.com>
2023-07-10 19:03:59 +0300

    EPHEH-73: Add color schema submodule.

---
+++ modules/oe_whitlabel_color_schema/src/EventSubscriber/ColorSchemaOptionsSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_whitelabel_color_schema\EventSubscriber;
+
+use Drupal\oe_whitelabel_color_schema\Event\ColorSchemaOptionsEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Provides options for the color schema fields.
+ *
+ * @see \Drupal\oe_paragraphs\EventSubscriber\OptionsSubscriber
+ * @see _oe_whitelabel_allowed_values_color_schema()
+ *
+ * @todo Put this into oe_showcase.
+ */
+class ColorSchemaOptionsSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      ColorSchemaOptionsEvent::class => ['getColorSchemaOptions', -1],
+    ];
+  }
+
+  /**
+   * Gets the icon options.
+   *
+   * @param \Drupal\oe_whitelabel_color_schema\Event\ColorSchemaOptionsEvent $event
+   *   Allowed format event object.
+   */
+  public function getIconOptions(ColorSchemaOptionsEvent $event): void {
+    $event->setColorSchemaOptions([
+      'oewt-color-schema-red' => 'Red',
+      'oewt-color-schema-blue' => 'Blue',
+    ]);
+  }
+
+}
