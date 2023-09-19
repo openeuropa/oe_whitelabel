675d826d51a780c40ac8a6968293f2d8f7e2674c
Julien Tekrane <julien.tekrane@laposte.net>
2023-09-19 16:29:01 +0000

    ELA-650: Move duplicated code to parent class.

--- modules/oe_whitelabel_link_lists/src/Plugin/LinkDisplay/ColumnLinkDisplayPluginBase.php
+++ modules/oe_whitelabel_link_lists/src/Plugin/LinkDisplay/ColumnLinkDisplayPluginBase.php
@@ -4,9 +4,15 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_whitelabel_link_lists\Plugin\LinkDisplay;
 
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Template\Attribute;
+use Drupal\Core\Url;
+use Drupal\oe_link_lists\EntityAwareLinkInterface;
 use Drupal\oe_link_lists\LinkCollectionInterface;
 use Drupal\oe_link_lists\LinkDisplayPluginBase;
 
@@ -16,15 +22,39 @@ use Drupal\oe_link_lists\LinkDisplayPluginBase;
 abstract class ColumnLinkDisplayPluginBase extends LinkDisplayPluginBase implements ContainerFactoryPluginInterface {
 
   /**
-   * Builds items.
+   * The entity repository.
    *
-   * @param \Drupal\oe_link_lists\LinkCollectionInterface $links
-   *   Links to be added in each column.
-   *
-   * @return array
-   *   The renderable array (using Pattern).
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
    */
-  abstract protected function buildItems(LinkCollectionInterface $links): array;
+  protected $entityRepository;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * ColumnLinkDisplayPluginBase constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, string $plugin_id, array $plugin_definition, EntityRepositoryInterface $entity_repository, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->entityRepository = $entity_repository;
+    $this->entityTypeManager = $entity_type_manager;
+  }
 
   /**
    * {@inheritdoc}
@@ -32,6 +62,8 @@ abstract class ColumnLinkDisplayPluginBase extends LinkDisplayPluginBase impleme
   public function defaultConfiguration() {
     return [
       'columns' => 1,
+      'equal_height' => TRUE,
+      'background_color' => 'bg-white',
     ] + parent::defaultConfiguration();
   }
 
@@ -56,11 +88,12 @@ abstract class ColumnLinkDisplayPluginBase extends LinkDisplayPluginBase impleme
     $form['background_color'] = [
       '#type' => 'select',
       '#title' => $this->t('Background color'),
+      // Backgroud HTML classes used in oe_whitelabel.
       '#options' => [
         'bg-white' => $this->t('White'),
         'bg-light' => $this->t('Light'),
       ],
-      '#default_value' => $this->configuration['background_color'] ?? 'white',
+      '#default_value' => $this->configuration['background_color'] ?? 'bg-white',
     ];
 
     return $form;
@@ -125,6 +158,56 @@ abstract class ColumnLinkDisplayPluginBase extends LinkDisplayPluginBase impleme
     ];
 
     return $build;
+  }
+
+  /**
+   * Builds items.
+   *
+   * @param \Drupal\oe_link_lists\LinkCollectionInterface $links
+   *   Links to be added in each column.
+   *
+   * @return array
+   *   The renderable array (using Pattern).
+   */
+  protected function buildItems(LinkCollectionInterface $links): array {
+    $items = [];
+
+    /** @var \Drupal\oe_link_lists\LinkInterface $link */
+    foreach ($links as $link) {
+      $entity = $link instanceof EntityAwareLinkInterface ? $link->getEntity() : NULL;
+
+      if (!$entity) {
+        // Skip invalid entities.
+        continue;
+      }
+
+      $entity = $this->entityRepository->getTranslationFromContext($entity);
+
+      // Check if the entity type has the view display available.
+      $entity_type_id = $entity->getEntityTypeId();
+      $bundle = $entity->bundle();
+      $view_display_id = $this->pluginId;
+      $storage = $this->entityTypeManager->getStorage('entity_view_display');
+      $view_display = $storage->load($entity_type_id . '.' . $bundle . '.' . $view_display_id);
+
+      if (!$view_display) {
+        $sub_path = $entity_type_id == 'node' ? 'types' : $entity->getEntityTypeId();
+        $link_here = Link::fromTextAndUrl($this->t('here'),
+          Url::fromUri('internal:' . '/admin/structure/' . $sub_path . '/manage/' . $bundle . '/display#edit-modes')
+        )->toString();
+        $message = new FormattableMarkup('The <b>@view_display</b> view mode is not available for <b>@bundle</b>. Please enable it @link_here', [
+          '@view_display' => $view_display_id,
+          '@bundle' => $entity->bundle(),
+          '@link_here' => $link_here,
+        ]);
+        \Drupal::messenger()->addError($message);
+        return $items;
+      }
+      $renderable_array = $this->entityTypeManager->getViewBuilder($entity_type_id)->view($entity, $view_display_id);
+
+      $items[] = ['content' => $renderable_array];
+    }
+    return $items;
   }
 
 }
--- modules/oe_whitelabel_link_lists/src/Plugin/LinkDisplay/TeaserDisplayPlugin.php
+++ modules/oe_whitelabel_link_lists/src/Plugin/LinkDisplay/TeaserDisplayPlugin.php
@@ -4,15 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_whitelabel_link_lists\Plugin\LinkDisplay;
 
-use Drupal\Component\Render\FormattableMarkup;
-use Drupal\Core\Entity\EntityRepositoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Link;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\Core\Url;
-use Drupal\oe_link_lists\EntityAwareLinkInterface;
-use Drupal\oe_link_lists\LinkCollectionInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -27,43 +19,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class TeaserDisplayPlugin extends ColumnLinkDisplayPluginBase implements ContainerFactoryPluginInterface {
 
-  use StringTranslationTrait;
-
-  /**
-   * The entity repository.
-   *
-   * @var \Drupal\Core\Entity\EntityRepositoryInterface
-   */
-  protected $entityRepository;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * ViewModeDisplayPlugin constructor.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin ID for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
-   *   The entity repository.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   */
-  public function __construct(array $configuration, string $plugin_id, array $plugin_definition, EntityRepositoryInterface $entity_repository, EntityTypeManagerInterface $entity_type_manager) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->entityRepository = $entity_repository;
-    $this->entityTypeManager = $entity_type_manager;
-  }
-
   /**
    * {@inheritdoc}
    */
@@ -75,48 +30,6 @@ class TeaserDisplayPlugin extends ColumnLinkDisplayPluginBase implements Contain
       $container->get('entity.repository'),
       $container->get('entity_type.manager')
     );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildItems(LinkCollectionInterface $links): array {
-    $items = [];
-
-    /** @var \Drupal\oe_link_lists\LinkInterface $link */
-    foreach ($links as $link) {
-      $entity = $link instanceof EntityAwareLinkInterface ? $link->getEntity() : NULL;
-
-      if (!$entity) {
-        // Skip invalid entities.
-        continue;
-      }
-
-      $entity = $this->entityRepository->getTranslationFromContext($entity);
-
-      // Check if the entity type has the view display available.
-      $entity_type_id = $entity->getEntityTypeId();
-      $bundle = $entity->bundle();
-      $view_display_id = $this->pluginId;
-      $storage = $this->entityTypeManager->getStorage('entity_view_display');
-      $view_display = $storage->load($entity_type_id . '.' . $bundle . '.' . $view_display_id);
-
-      if (!$view_display) {
-        $sub_path = $entity_type_id == 'node' ? 'types' : $entity->getEntityTypeId();
-        $link_here = Link::fromTextAndUrl($this->t('here'),
-          Url::fromUri('internal:' . '/admin/structure/' . $sub_path . '/manage/' . $bundle . '/display#edit-modes')
-        )->toString();
-        $message = new FormattableMarkup('The <b>@view_display</b> view mode is not available for <b>@bundle</b>. Please enable it @link_here', [
-          '@view_display' => $view_display_id,
-          '@bundle' => $entity->bundle(),
-          '@link_here' => $link_here,
-        ]);
-        \Drupal::messenger()->addError($message);
-        return $items;
-      }
-      $items[] = ['content' => $this->entityTypeManager->getViewBuilder($entity_type_id)->view($entity, $view_display_id)];
-    }
-    return $items;
   }
 
 }
