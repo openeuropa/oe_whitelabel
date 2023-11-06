b75f6b4d04f8ffc29895390433599d335f7d97de
Francesco Sardara <francesco@tdgwebservices.com>
2023-11-06 19:14:17 +0100

    ELA-650: Remove layout-specific implementations from plugin.

--- modules/oe_whitelabel_link_lists/src/Plugin/LinkDisplay/ColumnLinkDisplayPluginBase.php
+++ modules/oe_whitelabel_link_lists/src/Plugin/LinkDisplay/ColumnLinkDisplayPluginBase.php
@@ -74,8 +74,6 @@ abstract class ColumnLinkDisplayPluginBase extends LinkDisplayPluginBase impleme
   public function defaultConfiguration() {
     return [
       'columns' => 1,
-      'equal_height' => TRUE,
-      'background_color' => 'bg-white',
     ] + parent::defaultConfiguration();
   }
 
@@ -91,23 +89,6 @@ abstract class ColumnLinkDisplayPluginBase extends LinkDisplayPluginBase impleme
       '#default_value' => $this->configuration['columns'] ?? 1,
     ];
 
-    $form['equal_height'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Equal height'),
-      '#default_value' => $this->configuration['equal_height'] ?? TRUE,
-    ];
-
-    $form['background_color'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Background color'),
-      // Backgroud HTML classes used in oe_whitelabel.
-      '#options' => [
-        'bg-white' => $this->t('White'),
-        'bg-light' => $this->t('Light'),
-      ],
-      '#default_value' => $this->configuration['background_color'] ?? 'bg-white',
-    ];
-
     return $form;
   }
 
@@ -117,44 +98,17 @@ abstract class ColumnLinkDisplayPluginBase extends LinkDisplayPluginBase impleme
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     parent::submitConfigurationForm($form, $form_state);
     $this->configuration['columns'] = $form_state->getValue('columns');
-    $this->configuration['equal_height'] = $form_state->getValue('equal_height');
-    $this->configuration['background_color'] = $form_state->getValue('background_color');
   }
 
   /**
    * {@inheritdoc}
    */
   public function build(LinkCollectionInterface $links): array {
-    $build = parent::build($links);
+    $build = [];
 
     $items = $this->buildItems($links);
     if (empty($items)) {
       return $build;
-    }
-
-    // Set additional attributes.
-    foreach ($items as &$item) {
-      $attributes = new Attribute($item['attributes'] ?? []);
-      // Equal height.
-      if (!empty($this->configuration['equal_height'])) {
-        // Parent wrapper.
-        $attributes->addClass('h-100');
-        // Child have to take the height of the parent.
-        if (!empty($item['content'])) {
-          $content_attributes = new Attribute($item['content']['attributes'] ?? []);
-          $content_attributes->addClass('h-100');
-        }
-      }
-      // Background color.
-      if (!empty($this->configuration['background_color'])) {
-        $content_attributes->addClass($this->configuration['background_color']);
-      }
-
-      // Set values.
-      $item['#attributes'] = $attributes->toArray();
-      if (!empty($item['content']) && !empty($content_attributes)) {
-        $item['content']['#attributes'] = $content_attributes->toArray();
-      }
     }
 
     // The content.
@@ -167,7 +121,6 @@ abstract class ColumnLinkDisplayPluginBase extends LinkDisplayPluginBase impleme
         '#id' => 'columns',
         '#columns' => $this->configuration['columns'],
         '#items' => $items,
-        '#attributes' => $attributes,
       ],
     ];
 
