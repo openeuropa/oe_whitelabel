e6f921e5fec351b295ee6209c2fafd5ca98a3945
Francesco Sardara <francesco@tdgwebservices.com>
2024-01-22 18:18:10 +0100

    OEL-2767: Add an action bar field group for content banner view modes.

--- modules/oe_whitelabel_extra_project/oe_whitelabel_extra_project.post_update.php
+++ modules/oe_whitelabel_extra_project/oe_whitelabel_extra_project.post_update.php
@@ -21,3 +21,11 @@ function oe_whitelabel_extra_project_post_update_00001(): void {
   ];
   ConfigImporter::importMultiple('module', 'oe_whitelabel_extra_project', '/config/post_updates/00001_decimal_budget_fields', $configs, TRUE);
 }
+
+/**
+ * Update the content banner project view mode to introduce field group.
+ */
+function oe_whitelabel_extra_project_post_update_00002(): void {
+  \Drupal::service('module_installer')->install(['field_group']);
+  ConfigImporter::importSingle('module', 'oe_whitelabel_extra_project', '/config/post_updates/00002_field_group', 'core.entity_view_display.node.oe_project.oe_w_content_banner');
+}
