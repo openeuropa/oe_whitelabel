608ee54731df9afd9ca2f6c477de0848325042e8
Francesco Sardara <francesco@tdgwebservices.com>
2024-01-22 18:18:10 +0100

    OEL-2767: Add an action bar field group for content banner view modes.

--- modules/oe_whitelabel_starter_news/oe_whitelabel_starter_news.post_update.php
+++ modules/oe_whitelabel_starter_news/oe_whitelabel_starter_news.post_update.php
@@ -19,3 +19,11 @@ function oe_whitelabel_starter_news_post_update_00001(): void {
   ];
   ConfigImporter::importMultiple('module', 'oe_whitelabel_starter_news', '/config/post_updates/00001_content_banner', $configs, TRUE);
 }
+
+/**
+ * Update the content banner news view mode to introduce field group.
+ */
+function oe_whitelabel_starter_news_post_update_00002(): void {
+  \Drupal::service('module_installer')->install(['field_group']);
+  ConfigImporter::importSingle('module', 'oe_whitelabel_starter_news', '/config/post_updates/00002_field_group', 'core.entity_view_display.node.oe_sc_news.oe_w_content_banner');
+}
---
+++ modules/oe_whitelabel_starter_person/oe_whitelabel_starter_person.post_update.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Contains install, update and uninstall functions.
+ */
+
+declare(strict_types = 1);
+
+use Drupal\oe_bootstrap_theme\ConfigImporter;
+
+/**
+ * Update the content banner person view mode to introduce field group.
+ */
+function oe_whitelabel_starter_person_post_update_00001(): void {
+  \Drupal::service('module_installer')->install(['field_group']);
+  ConfigImporter::importSingle('module', 'oe_whitelabel_starter_person', '/config/post_updates/00001_field_group', 'core.entity_view_display.node.oe_sc_person.oe_w_content_banner');
+}
