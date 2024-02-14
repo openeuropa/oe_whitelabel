c1520c5286367ff75ee584c04813f125237e64d6
AaronGilMartinez <aaron.gil.martinez@nttdata.com>
2024-02-14 16:49:07 +0100

    OEL-2504: Fix update folder name.

--- modules/oe_whitelabel_list_pages/oe_whitelabel_list_pages.post_update.php
+++ modules/oe_whitelabel_list_pages/oe_whitelabel_list_pages.post_update.php
@@ -20,7 +20,7 @@ function oe_whitelabel_list_pages_post_update_00001() {
  * Add content banner to list pages.
  */
 function oe_whitelabel_list_pages_post_update_00002() {
-  ConfigImporter::importMultiple('module', 'oe_whitelabel_list_pages', '/config/post_updates/00002_title', [
+  ConfigImporter::importMultiple('module', 'oe_whitelabel_list_pages', '/config/post_updates/00002_content_banner', [
     'core.entity_view_display.node.oe_list_page.default',
     'core.entity_view_display.node.oe_list_page.full',
     'field.field.node.oe_list_page.oe_featured_media',
