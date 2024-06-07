cc16a2d993a0d8de62c4a8d3f9b7e20729715aaa
drishu <aszilagyi@live.com>
2024-06-07 20:35:25 +0300

    EPHEH-465: Replace background parameter with options.

--- src/ColorSchemePreprocess.php
+++ src/ColorSchemePreprocess.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\oe_whitelabel;
 
 use Drupal\Component\Utility\Html;
+use Drupal\Core\Template\Attribute;
 
 /**
  * Class containing color scheme preprocesses.
@@ -19,16 +20,26 @@ class ColorSchemePreprocess {
    * @param bool $background
    *   Add a background class if TRUE.
    */
-  public function injectColorScheme(array &$variables, bool $background = TRUE): void {
+  public function injectColorScheme(array &$variables, array $options = []): void {
     if (!isset($variables['elements']['#oe_color_scheme'])) {
       return;
     }
 
-    $variables['attributes']['class'][] = Html::getClass($variables['elements']['#oe_color_scheme']);
-    $variables['attributes']['class'][] = 'text-color-default';
+    $variables['attributes'] = new Attribute($variables['attributes'] ?? []);
+    $variables['attributes']->addClass(Html::getClass($variables['elements']['#oe_color_scheme']));
 
-    if ($background) {
-      $variables['attributes']['class'][] = 'bg-default';
+    if ($options['text_colored']) {
+      $variables['attributes']->addClass('text-color-default');
+    }
+
+    if ($options['background']) {
+      $variables['attributes']->addClass('bg-default');
+    }
+
+    if ($options['primary_background']) {
+      $variables['attributes']->addClass('text-bg-primary');
+      // The text-bg- class already sets the text and background color.
+      $variables['attributes']->removeClass(['bg-default', 'text-color-default']);
     }
   }
 
