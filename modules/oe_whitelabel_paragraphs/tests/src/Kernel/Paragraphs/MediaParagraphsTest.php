845351d86978496e43f5967a4cfb73fe58a5fbb0
Piotr Smykaj <piotr.smykaj@gmail.com>
2025-03-12 21:58:48 +0100

    OEL-3341: Handle link access in banner paragraph button.

--- modules/oe_whitelabel_paragraphs/tests/src/Kernel/Paragraphs/MediaParagraphsTest.php
+++ modules/oe_whitelabel_paragraphs/tests/src/Kernel/Paragraphs/MediaParagraphsTest.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace Drupal\Tests\oe_whitelabel_paragraphs\Kernel\Paragraphs;
 
 use Drupal\Core\Url;
+use Drupal\Tests\node\Traits\ContentTypeCreationTrait;
+use Drupal\Tests\user\Traits\UserCreationTrait;
 use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * Tests the rendering of paragraph types with media fields.
  */
 class MediaParagraphsTest extends ParagraphsTestBase {
+
+  use ContentTypeCreationTrait;
+  use UserCreationTrait;
 
   /**
    * {@inheritdoc}
@@ -28,8 +33,13 @@ class MediaParagraphsTest extends ParagraphsTestBase {
     $this->container->get('module_handler')->loadInclude('oe_paragraphs_media_field_storage', 'install');
     oe_paragraphs_media_field_storage_install(FALSE);
     $this->installEntitySchema('media');
+    $this->installEntitySchema('node');
+    $this->installEntitySchema('user');
+    $this->installSchema('node', ['node_access']);
     $this->installConfig([
       'media',
+      'node',
+      'user',
       'oe_media',
       'oe_paragraphs_media',
       'media_avportal',
@@ -42,6 +52,12 @@ class MediaParagraphsTest extends ParagraphsTestBase {
     // Call the install hook of the Media module.
     $this->container->get('module_handler')->loadInclude('media', 'install');
     media_install();
+
+    $this->createContentType([
+      'type' => 'paragraphs_test',
+      'name' => 'Paragraphs Test',
+    ]);
+    $this->setCurrentUser($this->createUser(['access content']));
   }
 
   /**
@@ -672,6 +688,30 @@ class MediaParagraphsTest extends ParagraphsTestBase {
       'url(' . (\Drupal::service('file_url_generator')->generateAbsoluteString('avportal://P-038924/00-15.jpg')) . ')',
       $image_element->attr('style')
     );
+
+    // Verify that link is not displayed if the user lacks access to the
+    // reference page.
+    $node_storage = $this->container->get('entity_type.manager')->getStorage('node');
+    $node = $node_storage->create([
+      'type' => 'paragraphs_test',
+      'title' => 'Example node',
+      'status' => 1,
+    ]);
+    $node->save();
+    $paragraph->get('field_oe_link')->setValue([
+      'uri' => 'entity:node/' . $node->id(),
+      'title' => 'Example node',
+    ]);
+    $paragraph->save();
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+    $this->assertStringContainsString('Example node', trim($crawler->filter('a.btn')->text()));
+    $node->setUnpublished();
+    $node->save();
+    \Drupal::entityTypeManager()->getAccessControlHandler('node')->resetCache();
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+    $this->assertCount(0, $crawler->filter('a.btn'));
   }
 
   /**
