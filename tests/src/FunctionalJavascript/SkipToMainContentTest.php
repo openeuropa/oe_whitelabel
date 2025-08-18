c6c0871b970d48e89f24b648997cbd31d7405df5
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2025-08-18 14:04:42 +0300

    OEL-3730: Changed id to main and removed anchor plus test.

---
+++ tests/src/FunctionalJavascript/SkipToMainContentTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\oe_whitelabel\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+
+/**
+ * Verifies the "Skip to main content" link focuses <main id="main-content">.
+ */
+class SkipToMainContentTest extends WebDriverTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'oe_whitelabel';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'oe_bootstrap_theme_helper',
+  ];
+
+  /**
+   * Ensures the skip link focuses the main landmark.
+   */
+  public function testSkipToMainContentFocusesMain(): void {
+    $this->drupalGet('<front>');
+    $assert = $this->assertSession();
+    $skip_link = $assert->elementExists('css', 'a[href="#main-content"]');
+    $this->getSession()->executeScript(<<<'JS'
+      (function () {
+        var a = document.querySelector('a[href="#main-content"]');
+        if (!a) return;
+        a.focus();
+        a.click();
+      }());
+    JS);
+    $hash = $this->getSession()->evaluateScript('return window.location.hash;');
+    $this->assertSame('#main-content', $hash, 'Location hash updated to #main-content.');
+    $active_id = $this->getSession()->evaluateScript('return document.activeElement ? document.activeElement.id : "";');
+    $this->assertSame('main-content', $active_id, 'Focus moved to #main-content.');
+    $main = $assert->elementExists('css', 'main#main-content');
+    $this->assertSame('-1', $main->getAttribute('tabindex'), '<main> has tabindex="-1".');
+    $this->assertCount(1, $this->getSession()->getPage()->findAll('css', 'main#main-content'));
+  }
+
+}
