bd71a42b0b1f28b16b8a3ea8d9cbf8b323a2c6a9
Andreas Hennings <andreas@dqxtech.net>
2025-11-04 20:51:20 +0100

    OEL-4180: Add phpstan.neon.php for version-specific ignores.

---
+++ phpstan.neon.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * This file contains version-specific PhpStan ignores.
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$config = [];
+
+if (version_compare(\Drupal::VERSION, '11.2', '>=')) {
+  // @todo Fix when we drop support for Drupal 10.
+  $config['parameters']['ignoreErrors'][] = [
+    'message' => '#^Call to deprecated function entity_test_create_bundle#',
+    'identifier' => 'function.deprecated',
+    'count' => 2,
+    // In a .neon.php file, paths must be absolute.
+    'path' => __DIR__ . '/modules/oe_whitelabel_link_lists/tests/src/Kernel/TeaserDisplayPluginListTest.php',
+  ];
+}
+
+return $config;
