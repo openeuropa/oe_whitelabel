c6bb6fe72dac421d9c6d30b07d54d4e47efd1c26
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2026-02-04 21:23:52 +0200

    OEL-4032: Updates for new icon improvements.

--- modules/oe_whitelabel_search/tests/src/Kernel/SearchBlockTest.php
+++ modules/oe_whitelabel_search/tests/src/Kernel/SearchBlockTest.php
@@ -230,7 +230,7 @@ class SearchBlockTest extends KernelTestBase {
     $this->assertStringContainsString('btn', $button->attr('class'));
     $this->assertStringContainsString('btn-primary', $button->attr('class'));
     $this->assertStringContainsString('btn-md', $button->attr('class'));
-    $icon = $button->filter('.bi.icon--s.me-md-2-5.bcl-search-form__btn_icon');
+    $icon = $button->filter('.bi.icon--s.bcl-search-form__btn_icon');
     $this->assertCount(1, $icon);
     $label = $button->filter('span.d-none.d-lg-inline-block');
     $this->assertCount(1, $label);
