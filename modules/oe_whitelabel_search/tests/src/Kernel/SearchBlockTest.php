c701e7f2121c606c60064ec10bf275623d25d50c
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2026-02-12 13:17:20 +0200

    OEL-4032: Cleanup class and fix tests.

--- modules/oe_whitelabel_search/tests/src/Kernel/SearchBlockTest.php
+++ modules/oe_whitelabel_search/tests/src/Kernel/SearchBlockTest.php
@@ -230,6 +230,7 @@ class SearchBlockTest extends KernelTestBase {
     $this->assertStringContainsString('btn', $button->attr('class'));
     $this->assertStringContainsString('btn-primary', $button->attr('class'));
     $this->assertStringContainsString('btn-md', $button->attr('class'));
+    $this->assertStringContainsString('gap-2-5', $button->attr('class'));
     $icon = $button->filter('.bi.icon--s.bcl-search-form__btn_icon');
     $this->assertCount(1, $icon);
     $label = $button->filter('span.d-none.d-lg-inline-block');
