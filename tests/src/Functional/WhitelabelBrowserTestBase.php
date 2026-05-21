bc554f365438d906f7789f8de842f2e62971d57d
Piotr Smykaj <piotr.smykaj@gmail.com>
2026-05-21 10:20:38 +0200

    OEL-4736: Add test for ContentProjectRenderTest.

--- tests/src/Functional/WhitelabelBrowserTestBase.php
+++ tests/src/Functional/WhitelabelBrowserTestBase.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace Drupal\Tests\oe_whitelabel\Functional;
 
 use Drupal\Tests\BrowserTestBase;
-use Drupal\field\Entity\FieldConfig;
-use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\Tests\oe_whitelabel\Traits\CckContainerTrait;
+use Drupal\Tests\oe_whitelabel\Traits\MediaCreationTrait;
 use Drupal\Tests\sparql_entity_storage\Traits\SparqlConnectionTrait;
 
 /**
@@ -17,6 +16,7 @@ abstract class WhitelabelBrowserTestBase extends BrowserTestBase {
 
   use SparqlConnectionTrait;
   use CckContainerTrait;
+  use MediaCreationTrait;
 
   /**
    * {@inheritdoc}
@@ -34,35 +34,6 @@ abstract class WhitelabelBrowserTestBase extends BrowserTestBase {
 
     // Without sparql, some blocks will cause problems.
     $this->setUpSparql();
-  }
-
-  /**
-   * Creates the media copyright field and attaches it to media bundles.
-   *
-   * @param array $bundles
-   *   The media bundles to attach the field to.
-   */
-  protected function createMediaCopyrightField(array $bundles = ['image']): void {
-    if (!FieldStorageConfig::loadByName('media', 'field_media_copyright')) {
-      FieldStorageConfig::create([
-        'field_name' => 'field_media_copyright',
-        'entity_type' => 'media',
-        'type' => 'string',
-      ])->save();
-    }
-
-    foreach ($bundles as $bundle) {
-      if (FieldConfig::loadByName('media', $bundle, 'field_media_copyright')) {
-        continue;
-      }
-
-      FieldConfig::create([
-        'field_name' => 'field_media_copyright',
-        'entity_type' => 'media',
-        'bundle' => $bundle,
-        'label' => 'Copyright',
-      ])->save();
-    }
   }
 
 }
